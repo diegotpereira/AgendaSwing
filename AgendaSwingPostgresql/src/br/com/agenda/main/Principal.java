dd7e90e3eb5fb8bb22b8fd945b8832e5eb764840
diego <diegoteixeirapereira@hotmail.com>
2020-01-11 20:23:17 -0300

    Teste inicial de coneção com banco de dados via método main

--- AgendaSwingPostgresql/src/br/com/agenda/main/Principal.java
+++ AgendaSwingPostgresql/src/br/com/agenda/main/Principal.java
@@ -1,5 +1,45 @@
 package br.com.agenda.main;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.agenda.conection.Conexao;
+
 public class Principal {
+	
+	public static void main(String[] args) {		
+		insere(2,"Diego Pereira");
+//		lista();		
+	}
+	
+	static void insere(int matricula, String nome) {
+		String sql = "INSERT INTO aluno (matricula,nome) VALUES ('"+matricula+
+				"','"+nome+"');";
+		Conexao con = new Conexao();
+		int res = con.executeUpdate(sql);
+		if (res >= 1) {
+			System.out.println("Inserção realizada!");
+		} else {
+			System.err.println("Inserção NÃO realizada!");
+		}
+	}
+ 
+	static void lista() {
+		String sql = "SELECT * FROM aluno";
+		Conexao con = new Conexao();
+				
+		System.out.println("Nome - Matrícula");
+		try {
+			ResultSet consulta = con.executeQuery(sql);
+			while(consulta.next()) {
+				int mat = consulta.getInt("matricula");
+				String nome = consulta.getString("nome");
+				System.out.println(nome+" - "+mat);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
 
 }
