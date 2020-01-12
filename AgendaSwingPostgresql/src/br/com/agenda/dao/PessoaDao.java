278bfe6ceac152580fc659b8202211eed9d31ce1
diego <diegoteixeirapereira@hotmail.com>
2020-01-12 17:04:13 -0300

    Definição do método inserir

--- AgendaSwingPostgresql/src/br/com/agenda/dao/PessoaDao.java
+++ AgendaSwingPostgresql/src/br/com/agenda/dao/PessoaDao.java
@@ -14,61 +14,47 @@ import br.com.agenda.models.Pessoa;
 
 public class PessoaDao implements Dao{
 	
+
+	
+	private Connection conn;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+    
+    
+    private final String SELECT=" SELECT * FROM EMPREGADO ";
+    private final String INSERT=" INSERT INTO TabFicha(Matricula, Nome, CPF, Telefone, Logradouro, Numero, Bairro, Cidade, Estado)VALUES(?,?,?,?,?,?,?,?,?) ";
+    private final String UPDATE=" UPDATE EMPREGADO SET NOME=?,DAT_NASC=?,SALARIO=? WHERE ID=? ";
+    private final String DELETE=" DELETE FROM EMPREGADO WHERE ID=? ";
+	
+	
+	
 	// Salva os dados no BD
 	@Override
 	public void insert(Pessoa pessoa) {
 		// TODO Auto-generated method stub
-		
-		try {
-			
-
-			// * Conexão como BD.
-			Connection con;
-
-			// * Objeto comdo SQL.
-			Statement stmt = con.createStatement();
-
-			// * Pega os dados informado no formulário,
-			
-			stmt.setInt(1,Integer.parseInt(entCodigo.getText()));
-			
-			//int cadMat = Integer.parseInt(entCodigo.getText());
-			String cadNome = entNome.getText();
-			String cadCPF = entCPF.getText();
-			String cadTelefone = entTelefone.getText();
-			String cadLogradouro = entLogradouro.getText();
-			int cadNumero = Integer.parseInt(entNumero.getText());
-			String cadBairro = entBairro.getText();
-			String cadCidade = entCidade.getText();
-			String cadEstado = entEstado.getText();
-
-			// * Insere os dados do formulário no BD.
-			stmt.executeUpdate("insert into TabFicha (Matricula, Nome, CPF, Telefone, Logradouro, Numero, Bairro, Cidade, Estado) values ('"
-					+ cadMat
-					+ "','"
-					+ cadNome
-					+ "','"
-					+ cadCPF
-					+ "','"
-					+ cadTelefone
-					+ "','"
-					+ cadLogradouro
-					+ "','"
-					+ cadNumero
-					+ "','"
-					+ cadBairro
-					+ "','"
-					+ cadCidade
-					+ "','"
-					+ cadEstado
-					+ "')");
-			JOptionPane.showMessageDialog(null, "Dados Salvos!");
-		
-		
-		
-
-		    
-		
+		  try {
+			    conn=Conexao.conectar();
+			    Conexao c = new Conexao();
+	            pstm=conn.prepareStatement(INSERT);
+	            pstm.setInt   (1, pessoa.getMatricula());
+	            pstm.setString(2, pessoa.getNome());
+	            pstm.setString(3, pessoa.getCPF());
+	            pstm.setString(4, pessoa.getTelefone());
+	            pstm.setString(5, pessoa.getLogradouro());
+	            pstm.setInt   (6, pessoa.getNumero());
+	            pstm.setString(7, pessoa.getBairro());
+	            pstm.setString(8, pessoa.getCidade());
+	            pstm.setString(9, pessoa.getEstado());
+	 
+	            pstm.executeUpdate();
+				       
+		  
+		                } catch (Exception e) {
+				            System.err.println("Ocorreu um erro, causa:"+e.getMessage());
+				            e.printStackTrace();
+				        }finally{
+				            Conexao.desconectar(conn, pstm, rs);
+				        }    
 	}
 
 	@Override
@@ -86,6 +72,105 @@ public class PessoaDao implements Dao{
 	@Override
 	public Object select(int id) {
 		// TODO Auto-generated method stub
+		
+//		boolean consulta = true;
+//
+//		try {
+//			// * Driver conector MySQL.
+//			Class.forName("org.postgresql.Driver");
+//
+//			// * Conexão BD
+//			Connection con = DriverManager.getConnection(
+//					"jdbc:postgresql://localhost:5432/Agenda", "postgres","1234");
+//
+//			Statement stmt = con.createStatement();
+//
+//			// * Entras de dados no campo codigo do formulário.
+//			int BuscaCodigo = Integer.parseInt(entBusca.getText());
+//			String BuscaCPF = entBusca.getText();
+//
+//			ResultSet RS = null;
+//
+//			// Escolha a Opção de Busca.
+//			if (opcaoBusca.getSelectedIndex() == 0) {
+//				JOptionPane.showMessageDialog(null,
+//						"Escolha uma Opção de Busca!");
+//
+//			} else if (opcaoBusca.getSelectedIndex() == 1) {
+//				RS = stmt
+//						.executeQuery("Select * from TabFicha where Matricula = "
+//								+ BuscaCodigo);
+//
+//				while (RS.next()) {
+//
+//					// * Exibe os valore retornados na consulta.
+//
+//					// * Pega a Matricula.
+//					int Mat = RS.getInt("Matricula");
+//
+//					// * Conver inteiro para String
+//					saidaCodigo.setText(String.valueOf(Mat));
+//
+//					// Pega os demais.
+//					saidaNome.setText(RS.getString("Nome"));
+//					saidaCPF.setText(RS.getString("CPF"));
+//					saidaTelefone.setText(RS.getString("Telefone"));
+//					saidaLogradouro.setText(RS.getString("Logradouro"));
+//					saidaNumero.setText(RS.getString("Numero"));
+//					saidaBairro.setText(RS.getString("Bairro"));
+//					saidaCidade.setText(RS.getString("Cidade"));
+//					saidaEstado.setText(RS.getString("Estado"));
+//
+//					consulta = false;
+//					JOptionPane.showMessageDialog(null, "Dados Encontrado!");
+//
+//				}
+//
+//			} else if (opcaoBusca.getSelectedIndex() == 2) {
+//				RS = stmt.executeQuery("Select * from TabFicha where CPF = "
+//						+ BuscaCPF);
+//
+//				while (RS.next()) {
+//
+//					// * Pega o CPF.
+//					String CPF = RS.getString("CPF");
+//					saidaCPF.setText(CPF);
+//
+//					// * Exibe os valore retornados na consulta.
+//					saidaNome.setText(RS.getString("Nome"));
+//					saidaCodigo.setText(RS.getString("Matricula"));
+//					saidaTelefone.setText(RS.getString("Telefone"));
+//					saidaLogradouro.setText(RS.getString("Logradouro"));
+//					saidaNumero.setText(RS.getString("Numero"));
+//					saidaBairro.setText(RS.getString("Bairro"));
+//					saidaCidade.setText(RS.getString("Cidade"));
+//					saidaEstado.setText(RS.getString("Estado"));
+//
+//					consulta = false;
+//					JOptionPane.showMessageDialog(null, "Dados Encontrado!");
+//
+//				}
+//
+//			}
+//
+//			if (consulta) {
+//				JOptionPane.showMessageDialog(null, "Dados não Encontrados!");
+//
+//			}
+//			RS.close();
+//			stmt.close();
+//
+//			// * Fecha conexão com DB.
+//			con.close();
+//
+//		} catch (SQLException Erro) {
+//			JOptionPane.showMessageDialog(null,
+//					"Erro Cmdo SQL" + Erro.getMessage());
+//
+//		} catch (ClassNotFoundException Erro) {
+//			JOptionPane.showMessageDialog(null, "Driver não Encontrado!");
+//
+//		}
 		return null;
 	}
 
