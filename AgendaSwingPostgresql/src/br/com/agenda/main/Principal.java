33566438ae4113eff939f5a50d05906593bd1ba2
diego <diegoteixeirapereira@hotmail.com>
2020-01-12 17:03:27 -0300

    Formatação da classe principal

--- AgendaSwingPostgresql/src/br/com/agenda/main/Principal.java
+++ AgendaSwingPostgresql/src/br/com/agenda/main/Principal.java
@@ -12,39 +12,5 @@ public class Principal {
 		
 		frmCadastro frm = new frmCadastro();
 		frm.setVisible(true);
-		
-//		insere(2,"Diego Pereira");
-//		lista();		
 	}
-	
-//	static void insere(int matricula, String nome) {
-//		String sql = "INSERT INTO aluno (matricula,nome) VALUES ('"+matricula+
-//				"','"+nome+"');";
-//		Conexao con = new Conexao();
-//		int res = con.executeUpdate(sql);
-//		if (res >= 1) {
-////			System.out.println("Inserção realizada!");
-//		} else {
-//			System.err.println("Inserção NÃO realizada!");
-//		}
-//	}
-// 
-//	static void lista() {
-//		String sql = "SELECT * FROM aluno";
-//		Conexao con = new Conexao();
-//				
-//		System.out.println("Nome - Matrícula");
-//		try {
-//			ResultSet consulta = con.executeQuery(sql);
-//			while(consulta.next()) {
-//				int mat = consulta.getInt("matricula");
-//				String nome = consulta.getString("nome");
-//				System.out.println(nome+" - "+mat);
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}	
-
 }
