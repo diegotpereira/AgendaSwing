b25ff36d3d3b5e1eff6b51132f8396a0691a112b
diego <diegoteixeirapereira@hotmail.com>
2020-01-12 17:07:20 -0300

    Alteração da classe de conexão ao banco de dados

--- AgendaSwingPostgresql/src/br/com/agenda/conection/Conexao.java
+++ AgendaSwingPostgresql/src/br/com/agenda/conection/Conexao.java
@@ -1,11 +1,15 @@
 package br.com.agenda.conection;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.sql.Connection;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import javax.swing.JOptionPane;
 
@@ -14,30 +18,71 @@ import javax.swing.JOptionPane;
 
 public class Conexao {
 	
-	public Conexao(){
+	
+	public static Connection conectar() throws Exception {
+		
+		Connection conec = null;
 	      
-	      
+	      try {
+	          Class.forName("org.postgresql.Driver");
+	          conec  = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Agenda", "postgres","1234");
+	      } catch (ClassNotFoundException | SQLException ex) {
+	          System.out.println(ex.getMessage());
+	      }
+	      return conec;
+	
+        
+     
     }
-    
-    Connection conec;
-  
-public Connection getCon(){
-      
-      try {
-          Class.forName("org.postgresql.Driver");
-          conec  = DriverManager.getConnection("jdbc:postgresql://localhost:5432/agenda", "postgres","1234");
-      } catch (ClassNotFoundException | SQLException ex) {
-          System.out.println(ex.getMessage());
-      }
-      return conec;
-  }  
- public void confirmar() throws SQLException {
-      try {
-          conec.commit();
-      } catch (SQLException e) {
-          throw new SQLException("Problemas na instrução SQL.\n" + e.getMessage());
-      } finally {
-          conec.close();
-      }
-  }
+ 
+    //sobrecarga dos metodo desconectar,
+    //na sobrecarga muda somente a assinatura dos metodos ou seja os parâmetros
+    public static void desconectar(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Exceção, causa:" + e.getClass());
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void desconectar(Connection conn, PreparedStatement pstm) {
+        try {
+            if (pstm != null) {
+                pstm.close();
+            }
+ 
+            if (conn != null) {
+                conn.close();
+            }
+ 
+        } catch (Exception e) {
+            System.out.println("Exceção, causa:" + e.getClass());
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void desconectar(Connection conn, PreparedStatement pstm, ResultSet rs) {
+        try {
+ 
+            if (rs != null) {
+                rs.close();
+            }
+ 
+            if (pstm != null) {
+                pstm.close();
+            }
+ 
+            if (conn != null) {
+                conn.close();
+            }
+ 
+        } catch (Exception e) {
+            System.out.println("Exceção, causa:" + e.getClass());
+            e.printStackTrace();
+ 
+        }
+    }
 }
