a388535f365ff39d7de5d091294dee93902d6260
diego <diegoteixeirapereira@hotmail.com>
2020-01-12 17:06:05 -0300

    Alteração do método de busca modificada

--- AgendaSwingPostgresql/src/br/com/agenda/view/frmBuscaModificada.java
+++ AgendaSwingPostgresql/src/br/com/agenda/view/frmBuscaModificada.java
@@ -5,18 +5,25 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class frmBuscaModificada extends JFrame {
 
@@ -125,5 +132,74 @@ public class frmBuscaModificada extends JFrame {
 		scrollPane.setViewportView(tabela);
 		panel.setLayout(gl_panel);
 	}
+	
+	// Busca dados no BD
+		public void BuscaDados() {
+			boolean consulta = true;
+			try {
+				Class.forName("org.postgresql.Driver");
+
+				Connection con = DriverManager.getConnection(
+						"jdbc:postgresql://localhost:5432/Agenda", "postgres","1234");
+
+				Statement stmt = con.createStatement();
+				ResultSet RS;
+
+				if (!entBusca.getText().equals("")) {
+					int consMatricula = Integer.parseInt(entBusca.getText());
+					RS = stmt
+							.executeQuery("Select * from TabFicha where Matricula="
+									+ consMatricula);
+
+				} else {
+					RS = stmt.executeQuery("Select * from TabFicha");
+
+				}
+
+				DefaultTableModel modelo = new DefaultTableModel();
+				modelo.addColumn("Codigo");
+				modelo.addColumn("Nome");
+				modelo.addColumn("CPF");
+				modelo.addColumn("Telefone");
+				modelo.addColumn("Logradouro");
+				modelo.addColumn("Numero");
+				modelo.addColumn("Bairro");
+				modelo.addColumn("Cidade");
+				modelo.addColumn("Estado");
+
+				while (RS.next()) {
+					int Mat = RS.getInt("Matricula");
+
+					modelo.addRow(new Object[] { String.valueOf(Mat),
+							RS.getString("Nome"), RS.getString("CPF"),
+							RS.getString("Telefone"), RS.getString("Logradouro"),
+							RS.getString("Numero"), RS.getString("Bairro"),
+							RS.getString("Cidade"), RS.getString("Estado") });
+					consulta = false;
+
+				}
+
+				tabela.setModel(modelo);
+
+				if (consulta) {
+					JOptionPane.showMessageDialog(null, "Dados Não Encontrados!");
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Dados Encontrado!");
+
+				}
+
+				RS.close();
+				stmt.close();
+				con.close();
+
+			} catch (SQLException Erro) {
+				JOptionPane.showMessageDialog(null,
+						"Erro Cmdo SQL" + Erro.getMessage());
+
+			} catch (Exception Erro) {
+				JOptionPane.showMessageDialog(null, "Driver não Encontrado!");
+			}
+		}
 
 }
